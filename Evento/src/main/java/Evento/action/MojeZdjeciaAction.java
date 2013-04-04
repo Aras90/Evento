8bc3d46e1b258fa826c32b84d5489b8019629efa
Arkadiusz Szymanski <aszymanski18@gmail.com>
2013-04-04 13:33:42 +0200

    najnowsze

---
+++ Evento/src/main/java/Evento/action/MojeZdjeciaAction.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package Evento.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Date;
+import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
+
+import java.util.List;
+import Evento.model.Picture;
+import Evento.model.Album;
+import Evento.bean.MainClass;
+
+/**
+ * 
+ */
+@Conversion()
+public class MojeZdjeciaAction extends ActionSupport {
+    
+    private Date now = new Date(System.currentTimeMillis());
+    private List<Picture> listaPictures;
+    private List<Album> albumList;
+    private String id;
+   
+    @TypeConversion(converter = "Evento.DateConverter")
+    public Date getDateNow() { return now; }
+    public List<Picture> getListaPictures() { return listaPictures;}
+    public List<Album> getListaAlbum() {return albumList; }
+    
+    public void setListaPictures(){
+    	
+    	System.err.println(getId());
+    	
+    }
+    
+    public String execute() throws Exception {
+        now = new Date(System.currentTimeMillis());
+        setListaPictures();
+        return SUCCESS;
+    }
+    
+    public void setId(String id){
+    	this.id = id;
+    	
+    }
+    
+    public String getId(){
+    	return this.id;
+    }
+    
+
+    
+}
--- Evento/src/main/webapp/jsp/poczekalnia.jsp
+++ Evento/src/main/webapp/jsp/poczekalnia.jsp
@@ -6,10 +6,26 @@
                 poczekalnia
                 <s:iterator value="albumList" status="stat"> 
                         <div class="folder"></div>
-                        treeee 	<s:property value="albumList [# stat.index][0].CreatedAt" />  <br>
-                        nazwa		<s:property value="albumList [# stat.index][1].Name" />   <br>
-                        			<s:property value="albumList [# stat.index][2]" />
-                        		
+                        
+                        <s:set name="Id_Album" value="albumList [# stat.index][0].Id_Album" /> 
+                        <a href="<s:url action="mojeZdjecia"><s:param name="id" value="#Id_Album" /> </s:url>">Obrazek folderu</a> <br>
+                        kiedy stworzono:	<s:property value="albumList [# stat.index][0].CreatedAt" />  <br>
+                        id:	<s:property value="albumList [# stat.index][0].Id_Album" />  <br>
+                        nazwa:		<s:property value="albumList [# stat.index][1].Name" />   <br>
+                        
+                     	
+                     	
+                     	
+                     	
+                     	
+                        
+                        
+                        
+                  
                 </s:iterator>
+                
+                
+                
+                  
         </body>
 </html>
